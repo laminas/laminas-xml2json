8ffbece9db8ca41fdd8b97986b257c56d8e934f8
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 12:05:43 -0600

    Rewriting as Laminas Project package

--- src/Exception/RecursionException.php
+++ src/Exception/RecursionException.php
@@ -1,11 +1,12 @@
 <?php
+
 /**
- * @link      http://github.com/zendframework/zend-xml2json for the canonical source repository
- * @copyright Copyright (c) 2005-2016 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       https://github.com/laminas/laminas-xml2json for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-xml2json/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-xml2json/blob/master/LICENSE.md New BSD License
  */
 
-namespace Zend\Xml2Json\Exception;
+namespace Laminas\Xml2Json\Exception;
 
 class RecursionException extends RuntimeException
 {
