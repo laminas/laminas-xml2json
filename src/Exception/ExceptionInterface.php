e5bbd99e073b8c8b05f2675640fe8b339fcf05a3
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 12:05:44 -0600

    Rewriting as Laminas Project package

--- src/Exception/ExceptionInterface.php
+++ src/Exception/ExceptionInterface.php
@@ -1,11 +1,12 @@
 <?php
+
 /**
- * @see       https://github.com/zendframework/zend-xml2json for the canonical source repository
- * @copyright Copyright (c) 2005-2018 Zend Technologies USA Inc. (https://www.zend.com)
- * @license   https://github.com/zendframework/zend-xml2json/blob/master/LICENSE.md New BSD License
+ * @see       https://github.com/laminas/laminas-xml2json for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-xml2json/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-xml2json/blob/master/LICENSE.md New BSD License
  */
 
-namespace Zend\Xml2Json\Exception;
+namespace Laminas\Xml2Json\Exception;
 
 interface ExceptionInterface
 {
